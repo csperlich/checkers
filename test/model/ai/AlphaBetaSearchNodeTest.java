a0e7b955dd3f776b8c96f150dfeeb58e88e215a2
Cade Sperlich <csperlich@gmail.com>
2015-10-12 19:45:52 -0400

    Remove testIsLeaf()

--- test/model/ai/AlphaBetaSearchNodeTest.java
+++ test/model/ai/AlphaBetaSearchNodeTest.java
@@ -1,8 +1,6 @@
 package model.ai;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -192,26 +190,6 @@ public class AlphaBetaSearchNodeTest {
 
         // check that expected equals actual
         assertEquals(expectedNodes, actualSearchNodes);
-    }
-
-    @Test
-    public void testIsLeaf() {
-        Board board = new Board();
-        int startingDepth = 0;
-        PieceColor startingColor = PieceColor.BLACK;
-        AlphaBetaSearchNode searchNode = new AlphaBetaSearchNode(board, startingDepth,
-                startingColor);
-        assertTrue(searchNode.isLeaf());
-
-        startingDepth = 1;
-        searchNode = new AlphaBetaSearchNode(board, startingDepth, startingColor);
-        assertFalse(searchNode.isLeaf());
-
-        startingDepth = 4;
-        board = new Board(Arrays.asList(1, 2, 3, 4, 5, 6), Arrays.asList());
-        searchNode = new AlphaBetaSearchNode(board, startingDepth, startingColor);
-        assertTrue(searchNode.isLeaf());
-
     }
 
 }
